922dc05114cc0886f02c5bf3aa6c4a5c718ae5ac
nuertey <38339811+nuertey@users.noreply.github.com>
2023-04-04 16:20:26 -0500

    Add files via upload

--- multiple_bot_asc.py
+++ multiple_bot_asc.py
@@ -49,19 +49,30 @@ def connect_to_mainasc(botasc_index):
 def do_communication_with_mainasc(socket_descriptor):
     while True:
         mainasc_message = socket_descriptor.recv(MESSAGE_SIZE_IN_BYTES)
+        print("Message received from MainASC:=> ")
         print(mainasc_message.decode("utf-8"))
         print()
         
         socket_descriptor.send(ACK_STRING)
+
+print("Here! In the main multiple_bot_asc program... ")
+print()
 
 # A more pythonic way of achieving loop-like behavior--list comprehensions.
 # And it is more efficient too! as it engenders and is interpreted into 
 # faster processing code. Thus, employ a list comprehension to simulate
 # the 500 BotASCs connecting up to the MainASC on the 500 sockets:
 multiple_botasc_socket_descriptors = [connect_to_mainasc(botasc_index) for botasc_index in botasc_indices] 
+
+print("Here are the socket descriptor numbers: ")
+print()
+
 print(multiple_botasc_socket_descriptors)
 print()
 
+# Now actually achieve the communication back and forth with the MainASC...
+# All 500 sockets each with its own thread of execution; to put it as
+# Rabelais would, "Each in its own eachery! :)"
 for socket_descriptor in multiple_botasc_socket_descriptors:
     single_botasc_thread_name = 'BotASCThread_' + str(socket_descriptor)
     single_botasc_thread = threading.Thread(target=do_communication_with_mainasc, name=single_botasc_thread_name, args=(socket_descriptor,))
@@ -70,6 +81,6 @@ for socket_descriptor in multiple_botasc_socket_descriptors:
     single_botasc_thread.daemon = True 
     single_botasc_thread.start()
 
-
-while True:
-    time.sleep(3600)
+print("Main context, here after the threads; nothing to do. Awaiting the\
+       multiple BotASC threads communicating continuously with the MainASC.")
+print()
